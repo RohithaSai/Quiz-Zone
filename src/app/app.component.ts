74947c0a0fe0271326f359914c6eea81071565ce
Lakshmidevi <lalshmidevi5@gmail.com>
2017-05-31 05:18:53 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,28 @@ import { Component } from '@angular/core';
 @Component({
 
   selector: 'app-root',
-  template: '<h1>Inline Template</h1>',
-  styles: ['h1{color: red;}'],
+  template: `
+        <h1>Text ({{textValue}})</h1>
+        <input #textbox type="text" [(ngModel)]="textValue" required> 
+        <button (click)="logText(textbox.value)">Update Log</button>
+        <button (click)="textValue=''">Clear</button>
+        
+        <h2>Template Reference Variable</h2>
+        Type: '{{textbox.type}}', required: '{{textbox.hasAttribute('required')}}',
+        upper: '{{textbox.value.toUpperCase()}}'
+        
+        <h2>Log <button (click)="log=''">Clear</button></h2>
+        <pre>{{log}}</pre>`
+,  styles: ['h1{color: red;}'],
 
 
 
 })
 export class AppComponent {
+private textValue = "initial value";
+    private log: string ='';
 
+    private logText(value: string): void {
+        this.log += `Text changed to '${value}'\n`
+    }
 }
